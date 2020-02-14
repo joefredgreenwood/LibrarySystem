0dad6c28dc79dceec72b5ad9e4f7478a2a5b9b98
joseph61288 <joseph61288@GB-LD1LPW10G019.mastek.com>
2020-02-14 11:37:40 +0000

    Members entity created

---
+++ library/src/main/java/com/example/library/api/MembersAPI.java
@@ -0,0 +1,5 @@
+package com.example.library.api;
+
+public interface MembersAPI {
+
+}
--- library/src/test/java/com/example/library/LibraryApplicationTests.java
+++ library/src/test/java/com/example/library/LibraryApplicationTests.java
@@ -7,7 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 class LibraryApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testMemberExample() {
+		
+		
+		
 	}
 
 }
